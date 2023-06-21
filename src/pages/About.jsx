2a31eacd6c33002d595f314183d1008500a31c4e
kasahun welela <getasewmequanint@cbe.com.et>
2023-06-21 15:54:54 +0300

    page responsiveness updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -31,22 +31,22 @@ function About() {
     },
   ];
   return (
-    <div className="">
+    <div className="mx-7">
       <h1 className="mt-32 mb-4 text-center text-2xl">
         About <span className="text-blue-600 font-semibold">Us</span>{" "}
       </h1>
-      <div className=" mx-7 flex flex-col gap-3  ">
+      <div className=" md:flex gap-3 md:w-3/4 mx-auto  ">
         <div>
-          <p className="font-bold text-center text-xl">
+          <p className="font-bold text-center md:text-start text-xl md:text-4xl">
             Where innovation fires inspiration and challenges the norm.
           </p>
         </div>
         <div>
-          <img className="md:w-2/4" src="./aboutUs.png" />
+          <img className="" src="./aboutUs.png" />
         </div>
       </div>
-      <div className="mx-7">
-        <h1>
+      <div className="text-center mt-3">
+        <h1 className="text-3xl">
           Who <span className="text-blue-600 font-semibold">we are...</span>{" "}
         </h1>
         <p className="mt-4">
@@ -59,7 +59,7 @@ function About() {
       </div>
 
       <h1 className="text-3xl font-bold text-center mt-5">Core Values</h1>
-      <div className="m-7 lg:mx-40 md:mx-24 md:grid grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="my-7 lg:mx-40 md:mx-24 md:grid grid-cols-2 lg:grid-cols-3 gap-4">
         {coreValue.map((value) => (
           <Card
             key={value.id}
@@ -70,55 +70,82 @@ function About() {
           />
         ))}
       </div>
-      <div className="mx-7">
-        <div>
-          <h1>Our Process</h1>
-          <img src="./processSmall.png" alt="process" />
-          <p>
-            Sit arcu, egestas nunc, eros dignissim nunc, pretium malesuada.
-            Tristique est tellus non maecenas in egestas aliquam. Eget dolor
-            pellentesque consequat donec lectus nisl ligula. Ut sed nisi amet.
-          </p>
+      <h1 className="mt-32 mb-4 text-center text-2xl font-bold">Our Process</h1>
+      <div className="md:flex gap-7 md:w-4/5 md:justify-center">
+        <div className="md:w-1/2 md:flex justify-end">
+          <img src="./processSmall.png" className="md:h-full " alt="process" />
         </div>
-        <div>
-          <div className="flex flex-col items-center gap-2">
-            <img src="./discovery.png" alt="icon" />
-            <h3 className="font-bold ">Discovery</h3>
-            <p className="text-justify">
-              Velit lacus ipsum, urna, pretium lacinia. Mauris fermentum ut nunc
-              est, nibh. Lectus eu vel et placerat sed velit morbi diam. Amet
-              malesuada eget aliquam imperdiet. Arcu dolor sed pretiu
+        <div className="md:w-1/2 basis-2/4  ">
+          <div>
+            <p>
+              Sit arcu, egestas nunc, eros dignissim nunc, pretium malesuada.
+              Tristique est tellus non maecenas in egestas aliquam. Eget dolor
+              pellentesque consequat donec lectus nisl ligula. Ut sed nisi amet.
             </p>
           </div>
-          <div className="flex flex-col items-center gap-2">
-            <img src="./plan.png" alt="icon" />
-            <h3 className="font-bold ">Plan</h3>
-            <p className="text-justify">
-              Tellus, lacus, sem adipiscing ac sem amet. Vitae proin volutpat
-              cras tempus vitae. Ipsum consectetur quis diam hendrerit pharetra
-              amet scelerisque. Elementum risus aliquam quam etiam. Eget eu
-              risus dui lacus, orci. Cras ultricies posuere adipiscing faucibu.
-            </p>
+          <div className="md:flex ">
+            <div className="flex justify-center ">
+              <img
+                src="./discovery.png"
+                className="md:h-12 md:w-24"
+                alt="icon"
+              />
+            </div>
+            <div className="md:ml-3">
+              <h3 className="font-bold text-center md:text-start ">
+                Discovery
+              </h3>
+              <p className="text-justify">
+                Velit lacus ipsum, urna, pretium lacinia. Mauris fermentum ut
+                nunc est, nibh. Lectus eu vel et placerat sed velit morbi diam.
+                Amet malesuada eget aliquam imperdiet. Arcu dolor sed pretiu
+              </p>
+            </div>
           </div>
-          <div className="flex flex-col items-center gap-2">
-            <img src="./execute.png" alt="icon" />
-            <h3 className="font-bold ">Execute</h3>
-            <p className="text-justify">
-              Congue ridiculus at tortor mattis turpis bibendum at pulvinar
-              viverra. Ultrices morbi amet quam nisl risus libero, sodales nibh
-              faucibus. Sed gravida elementum auctor fermentum id sem
-            </p>
+
+          <div className="md:flex">
+            <div className="flex justify-center">
+              <img src="./plan.png" className="md:h-12 md:w-24" alt="icon" />
+            </div>
+            <div className="md:ml-3">
+              <h3 className="font-bold text-center md:text-start">Plan</h3>
+              <p className="text-justify">
+                Tellus, lacus, sem adipiscing ac sem amet. Vitae proin volutpat
+                cras tempus vitae. Ipsum consectetur quis diam hendrerit
+                pharetra amet scelerisque. Elementum risus aliquam quam etiam.
+                Eget eu risus dui lacus, orci. Cras ultricies posuere adipiscing
+                faucibu.
+              </p>
+            </div>
           </div>
-          <div className="flex flex-col items-center gap-2">
-            <img src="./deliver.png" alt="icon" />
-            <h3 className="font-bold ">Deliver</h3>
-            <p className="text-justify">
-              Pellentesque id pharetra, semper neque purus. Ante lacinia in ut
-              sagittis sed sapien. In facilisi in a diam. Pellentesque arcu
-              faucibus vel ornare pulvinar sollicitudin eu. Tempus nisi
-              malesuada convallis velit viverra odio. Senectus consectetur fames
-              in sed velit, ornare. Sed arcu.
-            </p>
+          <div className="md:flex">
+            <div className="flex justify-center">
+              <img src="./execute.png" className="md:h-12 md:w-24" alt="icon" />
+            </div>
+
+            <div className="md:ml-3">
+              <h3 className="font-bold text-center md:text-start">Execute</h3>
+              <p className="text-justify">
+                Congue ridiculus at tortor mattis turpis bibendum at pulvinar
+                viverra. Ultrices morbi amet quam nisl risus libero, sodales
+                nibh faucibus. Sed gravida elementum auctor fermentum id sem
+              </p>{" "}
+            </div>
+          </div>
+          <div className="md:flex">
+            <div className="flex justify-center">
+              <img src="./deliver.png" className="md:h-12 md:w-24" alt="icon" />
+            </div>
+            <div className="md:ml-3">
+              <h3 className="font-bold text-center md:text-start">Deliver</h3>
+              <p className="text-justify">
+                Pellentesque id pharetra, semper neque purus. Ante lacinia in ut
+                sagittis sed sapien. In facilisi in a diam. Pellentesque arcu
+                faucibus vel ornare pulvinar sollicitudin eu. Tempus nisi
+                malesuada convallis velit viverra odio. Senectus consectetur
+                fames in sed velit, ornare. Sed arcu.
+              </p>
+            </div>
           </div>
         </div>
       </div>
