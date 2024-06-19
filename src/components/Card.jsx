e2f7ed81f36d4dab119b1686707f83358f2a354e
kasahun-welela <kasahunwelela1@gmail.com>
2024-06-19 14:32:41 +0300

    remove arrow

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,16 +3,16 @@ import React from "react";
 function Card(prop) {
   return (
     <>
-      <div className=" p-10 flex flex-col gap-5 justify-around items-center shadow-2xl rounded-2xl hover:bg-blue-600 hover:text-white">
+      <div className=" p-10 flex flex-col gap-5 justify-around items-center shadow-2xl rounded-2xl hover:bg-primary hover:text-white">
         <div className={`rounded-full w-16 ${prop.cssClass}  p-3 hover:white`}>
           <img src={prop.img} />
         </div>
         <h1 className="font-bold text-center text-xl">{prop.title}</h1>
         <p className="text-center">{prop.description}</p>
         {prop.arrow && (
-          <div className="rounded-full w-10  bg-gray-200 p-3">
+          <div className="rounded-full w-10  bg-gray-200 px-3 py-2">
             <a href="#">
-              <img src={prop.arrow} alt="arrow" />
+              <i className="ri-arrow-right-line"></i>
             </a>
           </div>
         )}
